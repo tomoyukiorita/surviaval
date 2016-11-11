d611cebd49d4a7ba6a496e2032f1d5ed513a4153
折田知之 <orita.tomoyuki@gmail.com>
2016-11-11 15:43:24 +0900

    survival

---
+++ Assets/Timer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	System.TimeSpan ts;
+
+	// Use this for initialization
+	void Start () {
+		ts = new System.TimeSpan(1,1,2,3,0);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		ts -= new System.TimeSpan(0,0,0,0,1000);
+
+		Debug.Log( ts.ToString() );
+		GetComponent<Text> ().text = ts.ToString();
+	}
+}
